c342e933a1245600e39115aebee35cf00918bf95
Sicro01 <simon.p.cropper@gmail.com>
2018-09-04 19:54:59 +0100

    updates

--- neural_net.py
+++ neural_net.py
@@ -1,6 +1,11 @@
 import numpy as np  # linear algebra
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+import missingno as msno
+# import matplotlib
+import matplotlib.pyplot as plt
+
 
 # Set ipython's max row, max columns and display width display settings
 pd.set_option('display.max_row', 1000)
@@ -24,226 +29,202 @@ pd.set_option('display.width', 200)
 #   5) Backward Propagation
 #   6) Update parameters
 
-# Define layer sizes
+def main():
+    np.random.seed(1)
 
-def initialise_parameters_deep(layer_dims):
-    np.random.seed(3)
-    parameters = {}
+    # Specify static variables
+    LEARNING_RATE = 0.075
+    NUMBER_ITERATIONS = 150000
 
-    L = len(layer_dims)  # number of layers in the network
-    print('No. of layers:', L)
-    for l in range(1, L):
-        print('Param set #:', l)
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
-        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+    #
+    # Specify columns to drop from training and test data
+    #
+    cols_to_drop = ['Name', 'Ticket', 'Cabin']
 
-    return parameters
+    #
+    # Import Challenge Training data and pre-process it
+    #
+    titanic_train = TitanicData()
+    titanic_train.load_data('../titanic/train.csv')
+    titanic_train.preprocess_data_new(cols_to_drop)
 
-def sigmoid(Z):
-    """
-       Arguments:
-       Z - numpy array of any shape
+    #
+    # Import Challenge Test data and pre-process it
+    #
+    titanic_test = TitanicData()
+    titanic_test.load_data('../titanic/test.csv')
+    titanic_test.preprocess_data_new(cols_to_drop)
 
-       Returns:
-       A - Output of sigmoid(Z) - same shape as Z
-       cache - Returns Z, useful for Back Propagation
-    """
+    # Split Challenge Training data into my own Train and Test dataframes
+    titanic_train_train = TitanicData()
+    titanic_train_test = TitanicData()
+    titanic_train_train.data, titanic_train_test.data = train_test_split(titanic_train.data, test_size=0.2)
 
-    cache = Z
-    A = 1 / (1 + np.exp(-Z))
-    return A, cache
+    # Set up the Features, Target variable and layer sizes
+    f_list = []
+    for t in range(1, len(titanic_train.data.columns)):
+        f_list.append(titanic_train.data.columns[t])
+    # Feature List, Target to predict, input layer size, output layer size, num hidden layers and sizes of hidden layers
+    titanic_features = Features(f_list, 'Survived', len(f_list), 1, 2, 6, 4)
 
+    # Set up the Neural Net Model for the Train Train dataset, specify if Train or Test model
+    titanic_train_train_model = NeuralNetModel(titanic_train_train, 'Train')
+    titanic_train_train_model.create_model_values(titanic_train_train_model.data_class, titanic_features)
 
-def relu(Z):
-    """
-    Implements RELU function
-    Arguments:
-    Z - Output of the linear layer, of any shape
+    # Set up the Neural Net Model for the Train Test dataset, specify if Train or Test model
+    titanic_train_test_model = NeuralNetModel(titanic_train_test, 'Train')
+    titanic_train_test_model.create_model_values(titanic_train_test_model.data_class, titanic_features)
 
-    Returns:
-    A - Post activation parameter, of the same shape as Z
-    cache - python dictionary containing "A", stored for computing the backward pass efficiently
-    """
-
-    A = np.maximum(0,Z)
-
-    assert(A.shape == Z.shape)
-
-    cache = Z
-    return Z, cache
+    # Set up the Neural Net Model for the Challenge Test dataset, specify if Train or Test model
+    titanic_test_model = NeuralNetModel(titanic_test, 'Test')
+    titanic_test_model.create_model_values(titanic_test_model.data_class, titanic_features)
 
 
-def linear_forward(A, W, b):
-    """
-    Implement the linear part of a layer's forward propagation
+class Features:
 
-    Arguments:
-        A - activations fom previous layer (or input data): (size of previous layer, number of examples)
-        W - weights matrix, numpy array of shape(size of current layer, size of previous layer)
-        b - bia vector, numpy array of shape (size of current layer, 1)
+    def __init__(self, f_list=None, target=None, input_layer_size=None, output_layer_size=None,
+                 number_hidden_layers=None, *hidden_layer_sizes):
+        self.feature_list = f_list or []
+        self.target_to_predict = target or None
+        self.input_layer_size = input_layer_size or []
+        self.output_layer_size = output_layer_size or []
+        self.number_hidden_layers = number_hidden_layers or []
+
+        # Set up the layer sizes of the neural net model
+        self.layer_dims = ([input_layer_size])
+        for i in range(number_hidden_layers):
+            self.layer_dims.append(hidden_layer_sizes[i])
+        self.layer_dims.append(self.output_layer_size)
+
+        if f_list is None:
+            self.features_loaded = False
+        else:
+            self.features_loaded = True
+
+
+class NeuralNetModel:
+
+    def __init__(self, data_class=None, model_type=None):
+        self.data_class = data_class or []
+        self.model_type = model_type or []
+        self.model_feature_values = None
+        self.model_target_values = None
+        self.parameters = None
+
+
+    def create_model_values(self, data_class, features):
+        self.model_feature_values = np.float64(data_class.data[features.feature_list].values)
+        self.model_feature_values = self.model_feature_values.T
+        self.initialise_parameters_deep(features.layer_dims)
+
+        if self.model_type == 'Train':
+            self.model_target_values = np.float64(data_class.data[features.target_to_predict].values)
+            self.model_target_values = self.model_target_values.T.reshape(1, self.model_target_values.shape[0])
+
+    def initialise_parameters_deep(self, layer_dims):
+        np.random.seed(3)
+        self.parameters = {}
+        l = len(layer_dims)  # number of layers in the network
+        for i in range(1, l):
+            self.parameters['W' + str(i)] = np.random.randn(layer_dims[i], layer_dims[i - 1]) * 0.01
+            self.parameters['b' + str(i)] = np.zeros((layer_dims[i], 1))
+
+
+class TitanicData:
+
+    def __init__(self):
+        self.data = None
+        self.data_loaded = False
+
+    def load_data(self, filename):
+        self.data = pd.read_csv(filename, index_col=0)
+        self.data_loaded = True
+
+    def preprocess_data_new(self, cols_to_drop):
+        self.drop_columns(cols_to_drop)
+        self.parse_gender_new()
+        self.parse_embarked_new()
+        self.parse_age_new()
+        self.parse_fare_new()
+        self.add_family_size_new()
+        self.drop_columns(['SibSp', 'Parch'])
+        self.data.FamilySize = self.normalize_new(self.data.FamilySize)
+        self.rename_col('FamilySize')
+        self.data.Age = self.normalize_new(self.data.Age)
+        self.rename_col('Age')
+        self.data.Fare = self.normalize_new(self.data.Fare)
+        self.rename_col('Fare')
+
+    def drop_columns(self, cols_to_drop):
+        self.data.drop(cols_to_drop, axis=1, inplace=True)
+
+    def parse_gender_new(self):
+        self.data.loc[self.data['Sex'] == 'male', 'Sex'] = 1
+        self.data.loc[self.data['Sex'] == 'female', 'Sex'] = 0
+
+    def parse_embarked_new(self):
+        # Set any NaN value to the most common value then assign integer values
+        self.data.loc[self.data['Embarked'] == "S", 'Embarked'] = 1.0
+        self.data.loc[self.data['Embarked'] == "C", 'Embarked'] = 2.0
+        self.data.loc[self.data['Embarked'] == "Q", 'Embarked'] = 3.0
+        self.data.Embarked = self.data.Embarked.fillna(self.data['Embarked'].median())
+
+    def parse_age_new(self):
+        median_age = self.data['Age'].median()
+        self.data['Age'] = self.data['Age'].fillna(median_age)
+
+    def parse_fare_new(self):
+        median_fare = self.data['Fare'].median()
+        self.data['Fare'] = self.data['Fare'].fillna(median_fare)
+
+    def add_family_size_new(self):
+        self.data['FamilySize'] = 0
+        self.data['FamilySize'] = self.data['Parch'] + self.data['SibSp']
+
+    def normalize_new(self, series):
+        series_mean = series.mean()
+        series_std = series.std()
+        return (series - series_mean) / series_std
+
+    def rename_col(self, col_name):
+        self.data.rename(columns={col_name: 'Normalized_' + col_name}, inplace=True)
+
+
+class HelperFunctions:
+
+    def sigmoid(Z):
+        """
+           Arguments:
+           Z - numpy array of any shape
+
+           Returns:
+           A - Output of sigmoid(Z) - same shape as Z
+           cache - Returns Z, useful for Back Propagation
+        """
+
+        cache = Z
+        A = 1 / (1 + np.exp(-Z))
+        return A, cache
+
+    def relu(Z):
+        """
+        Implements RELU function
+        Arguments:
+        Z - Output of the linear layer, of any shape
 
         Returns:
-        Z - the input of the activation function, also called the pre-activation parameter
-        cache - a python dictonary containing "A", "W" and "b": stored for computing the backwars pass efficiently
-    """
+        A - Post activation parameter, of the same shape as Z
+        cache - python dictionary containing "A", stored for computing the backward pass efficiently
+        """
 
-    Z = np.dot(W, A) + b
-    cache = (A, W, b)
+        A = np.maximum(0, Z)
 
-    return Z, cache
+        assert (A.shape == Z.shape)
 
-
-def linear_activation_forward(A_prev, W, b, activation):
-    """
-    Implement the forward propagation for the LINEAR -> ACTIVATION layer
-
-    Arguments:
-    A_prev - activations from previous layer (or input data): (suze of previous layer, number of examples)
-    W - weights matrix: numpy array of shape (size of current layer, size of previous layer)
-    b - bias vector: numpy array of shape (size of current layer, 1)
-    activation - the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
-    Returns:
-    A - the output of the activation function, also called the post-activation value
-    cache -  a python dictionary containing "linear_cache" and "activation_cache"; stored for computing the backward
-    pass efficiently
-    """
-
-    if activation == 'sigmoid':
-        Z, linear_cache = linear_forward(A_prev, W, b)
-        Z = np.float64(Z)
-        A, activation_cache = sigmoid(Z)
-
-    elif activation == 'relu':
-        Z, linear_cache = linear_forward(A_prev, W, b)
-        Z = np.float64(Z)
-        A, activation_cache = relu(Z)
-
-    cache = (linear_cache, activation_cache)
-    return A, cache
+        cache = Z
+        return Z, cache
 
 
-def L_model_forwards(X, parameters):
-    """
-    Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
+# Application Entry point -> call main()
+main()
 
-    Arguments:
-    X - data, numpy array pf shape (input size, number of examples)
-    parameters - output of initialize_parameters_deep()
-
-    Returns:
-    AL - last post-activation value
-    caches - list of caches containing:
-        every cache of liner_relu_forward() (there are L-1 of them, indexed from 0 - L-2)
-        the cache of linear_sigmoid_forward() (there is one indexed at L-1)
-    """
-
-    caches = []
-    A = X
-    L = len(parameters) // 2        # number of layers in the neural network
-
-    for l in range(1,L):
-        A_prev = A
-        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)],
-                                             parameters['b' + str(l)], 'relu')
-        caches.append(cache)
-
-        return
-
-
-def parse_gender(df):
-    df.loc[df['Sex'] == 'male', 'Sex'] = 1
-    df.loc[df['Sex'] == 'female', 'Sex'] = 0
-    return df
-
-
-def parse_embarked(df):
-    # Set any NaN value to the most common value then assign integer values
-    # df.Embarked = df.Embarked.fillna(df['Embarked'].value_counts(dropna=False).index[0])
-
-    df.loc[df['Embarked'] == "S", 'Embarked'] = 1.0
-    df.loc[df['Embarked'] == "C", 'Embarked'] = 2.0
-    df.loc[df['Embarked'] == "Q", 'Embarked'] = 3.0
-    df.Embarked = df.Embarked.fillna(df['Embarked'].median())
-    return df
-#    df.Embarked = df.Embarked.replace({
-#        'S': 1.0,
-#        'C': 2.0,
-#        'Q': 3.0
-#    })
-
-
-def parse_age(df):
-    median_age = df['Age'].median()
-    df['Age'] = df['Age'].fillna(median_age)
-    return df
-
-
-def add_family_size(df):
-    df['FamilySize'] = 0
-    df['FamilySize'] = df['Parch'] + df['SibSp']
-    return df
-
-
-def preprocess_data(df):
-    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
-    df = parse_gender(df)
-    df = parse_embarked(df)
-    df = parse_age(df)
-    df = add_family_size(df)
-    df.Age = normalize(df.Age)
-    df.FamilySize = normalize(df.FamilySize)
-    df = df.rename(columns={'Age': 'NormalizedAge', 'FamilySize': 'NormalizedFamilySize'})
-    return df
-
-
-def normalize(series):
-    series_mean = series.mean()
-    series_std = series.std()
-    return (series - series_mean) / series_std
-
-
-np.random.seed(1)
-
-# Import Challenge Training and Test data
-challenge_train_df = pd.read_csv('../titanic/train.csv', index_col=0)
-challenge_test_df = pd.read_csv('../titanic/test.csv', index_col=0)
-
-# Pre-process Challenge Training and Test data
-challenge_train_df = preprocess_data(challenge_train_df)
-challenge_test_df = preprocess_data(challenge_test_df)
-
-# Split Challenge Training data into my own Train and Test dataframes
-my_train_df, my_test_df = train_test_split(challenge_train_df, test_size=0.2)
-
-# Set up features list - these are the data value to be fed into the nn model
-features_list = 'Pclass Sex NormalizedAge Fare Embarked NormalizedFamilySize'.split()
-
-# Use features list to set up features and target for my training and test data
-my_train_features = np.float64(my_train_df[features_list].values)
-my_train_target = np.float64(my_train_df['Survived'].values)
-my_test_features = np.float64(my_test_df[features_list].values)
-my_test_target = np.float64(my_test_df['Survived'].values)
-
-# Use Features List to set up features and values for the Challenge data (to predict)
-challenge_test_features = np.float64(challenge_test_df[features_list].values)
-
-# Set up layer size variable and transpose my training data features and adjust shape of target matrix
-layers_dims = [6, 6, 4, 1]      # 1 input layer for the 6 input vaiables, 2 hidden layers and 1 output layer
-my_train_features_t = my_train_features.T
-my_train_target_t = my_train_target.T.reshape(1, my_train_target.shape[0])
-
-# Transpose my test data features and adjust shape of target matrix
-my_test_features = my_test_features.T
-my_test_target_t = my_test_target.T.reshape(1, my_test_target.shape[0])
-
-# Transpose the Challenge test data features
-challenge_test_features_t = challenge_test_features.T
-
-# Define layers and initialise Weights and biases
-parameters = initialise_parameters_deep([6, 6, 4, 1])
-print("L:", len(parameters))
-
-
-# Forward propagation
-# Z, linear_cache = linear_forward(A,W,b)
